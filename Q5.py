c74407f4d3ae56057b763a8aeea2a6b82e9cdac3
vivekmudi <mudiramavenkatavivek@gmail.com>
2025-12-29 21:59:04 +0530

    data types

---
+++ Q5.py
@@ -0,0 +1,12 @@
+n1=int(input("Enter n1: "))
+n2=int(input("Enter n2: "))
+print(f"sum is {n1+n2}")
+print(f"difference is {n1-n2}")
+print(f"product is {n1*n2}")
+if n1==n2:
+    print("n1 and n2 are equal")
+else:
+      if n1>n2:
+          print(f"{n1} is big")
+      else:
+          print(f"{n2} is big")
